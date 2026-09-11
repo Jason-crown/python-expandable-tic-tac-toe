a6a2c95545e9fc3b9a0f50815dfd7a827bb32c00
jason klutse <jklutse@uoguelph.ca>
2026-09-10 23:19:03 -0400

    Removed the test file

--- test.py
+++
@@ -1,7 +0,0 @@
-horizontal_check = 0
-for rows in range(0, 5):
-    for columns in range(0, 5):
-        print(f"{rows}, {columns}")
-        horizontal_check += 1
-    print(f"----{horizontal_check}")
-    horizontal_check = 0
