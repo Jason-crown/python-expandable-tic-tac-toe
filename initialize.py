e972d1f41419597233768d17a2771ae1ea85cc96
jason klutse <jklutse@uoguelph.ca>
2026-09-11 00:01:11 -0400

    width fix for terminal

--- initialize.py
+++ initialize.py
@@ -5,8 +5,8 @@ print(f"width = {width}, height = {height}")
 
 x = 3
 y = 3
-if (width - 5 < x): 
-    x = width - 5
+if (width/2 - 5 < x): 
+    x = int(width/2) - 5
     print(f"new width = {width - 5}")
 if (height - 5 < y): 
     y = height - 5
