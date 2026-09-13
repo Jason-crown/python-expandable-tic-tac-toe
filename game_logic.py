f6277fe497aa9d7273bf877a98f7839a5a63170c
jason klutse <jklutse@uoguelph.ca>
2026-09-13 18:59:35 -0400

    save point for larger string based changes

--- game_logic.py
+++ game_logic.py
@@ -2,19 +2,22 @@ import sys
 from initialize import game1
 
 def check_win(board, player, x, y):
-    if not any(game1.space in row for row in board):
-        print("\x1b[3J\x1b[H\x1b[2J Tie")
     for span in range(abs(y-x)+1):
         for types in range(2):
             if x > y and span > 0:
                 span *= -1
             line_check(types, player, board, x, y, span)
+    if not any(game1.space in row for row in board):
+        print("\x1b[3J\x1b[H\x1b[2J Tie")
+    
 
 def line_check(type, player, board, x, y, span):
     
     line_set = diagonal_set = 0
     sub_board = [[row[i] for row in board] for i in range(len(board[0]))]
     line_list = [player]*game1.win_amount
+    diagonal_list = []
+    anti_diagonal_list = []
 
     if type == 0:
         upper = x
@@ -41,12 +44,21 @@ def line_check(type, player, board, x, y, span):
             if ("".join(line_list) in "".join(board[rows]) or "".join(line_list) in "".join(sub_board[columns])) == False:
                 line_set = 0
             
-            if columns == diagonal_logic+span and board[rows][columns] == player:
-                diagonal_set += 1
+            if columns == diagonal_logic+span:
+                if type == 0:
+                    diagonal_list.append(board[columns][diagonal_logic+span])
+                else :
+                    anti_diagonal_list.append(board[columns][diagonal_logic+span])
+
+                if board[rows][columns] == player:
+                    diagonal_set += 1
+                #if ("".join(line_list) in "".join(anti_diagonal_list) or "".join(line_list) in "".join(diagonal_list)) == True:
+                    #print(f"{anti_diagonal_list}")
+            
             
                 
         if game1.win_amount in (line_set, diagonal_set) or line_set > game1.win_amount:
-            print(f"\x1b[3J\x1b[H\x1b[2J {player} WINS")
+            print(f"\x1b[3J\x1b[H\x1b[2J {player} WINS{diagonal_list}")
         line_set = 0
 
 
