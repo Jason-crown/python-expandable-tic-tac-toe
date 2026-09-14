6a3a44aa4380646f30136157bc273d11e51ff3b0
jason klutse <jklutse@uoguelph.ca>
2026-09-13 21:41:37 -0400

    reverted code to try again

--- game_logic.py
+++ game_logic.py
@@ -2,22 +2,19 @@ import sys
 from initialize import game1
 
 def check_win(board, player, x, y):
+    if not any(game1.space in row for row in board):
+        print("\x1b[3J\x1b[H\x1b[2J Tie")
     for span in range(abs(y-x)+1):
         for types in range(2):
             if x > y and span > 0:
                 span *= -1
             line_check(types, player, board, x, y, span)
-    if not any(game1.space in row for row in board):
-        print("\x1b[3J\x1b[H\x1b[2J Tie")
-    
 
 def line_check(type, player, board, x, y, span):
     
     line_set = diagonal_set = 0
     sub_board = [[row[i] for row in board] for i in range(len(board[0]))]
     line_list = [player]*game1.win_amount
-    diagonal_list = []
-    anti_diagonal_list = []
 
     if type == 0:
         upper = x
@@ -46,22 +43,12 @@ def line_check(type, player, board, x, y, span):
             else:
                 print(f" {player} WINS1")
             
-            if columns == diagonal_logic+span:
-                if type == 0:
-                    diagonal_list.append(board[columns][diagonal_logic+span])
-                else :
-                    anti_diagonal_list.append(board[columns][diagonal_logic+span])
-
-                if board[rows][columns] == player:
-                    diagonal_set += 1
-                if ("".join(line_list) in "".join(anti_diagonal_list) or "".join(line_list) in "".join(diagonal_list)) == True:
-                    print(f" {player} WINS2")
-                    
-            
+            if columns == diagonal_logic+span and board[rows][columns] == player:
+                diagonal_set += 1
             
                 
         if game1.win_amount in (line_set, diagonal_set) or line_set > game1.win_amount:
-            print(f"\x1b[3J\x1b[H\x1b[2J {player} WINS3")
+            print(f"\x1b[3J\x1b[H\x1b[2J {player} WINS")
         line_set = 0
 
 
