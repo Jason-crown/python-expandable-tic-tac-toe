8ee5de0635aca7db298785e84e45e563214ef3b4
jason klutse <jklutse@uoguelph.ca>
2026-09-14 23:43:11 -0400

    worked on diagonal logic

--- game_logic.py
+++ game_logic.py
@@ -50,7 +50,10 @@ def line_check(type, player, board, x, y, span):
             if columns == set_2 and type == 1:
                 print(columns, diagonal_logic+span)
                 anti_diagonal_list.append(board[diagonal_logic+span][columns])
-
+            if ("".join(line_list) in "".join(anti_diagonal_list) or "".join(line_list) in "".join(diagonal_list)) == False:
+                line_set = 0
+            else:
+                print(f" {player} WINS1")
             if columns == diagonal_logic+span and board[rows][columns] == player:
                 diagonal_set += 1
                 
