add21ed91e5e6acae2fb7ab1949fd39ca27c51f4
jason klutse <jklutse@uoguelph.ca>
2026-09-14 23:44:24 -0400

    set diagonal set to 0 when not a win

--- game_logic.py
+++ game_logic.py
@@ -46,12 +46,12 @@ def line_check(type, player, board, x, y, span):
 
             if columns == diagonal_logic and type == 0: 
                 diagonal_list.append(board[diagonal_logic+span][columns])
-                print(columns, diagonal_logic+span)
+                #print(columns, diagonal_logic+span)
             if columns == set_2 and type == 1:
-                print(columns, diagonal_logic+span)
+                #print(columns, diagonal_logic+span)
                 anti_diagonal_list.append(board[diagonal_logic+span][columns])
             if ("".join(line_list) in "".join(anti_diagonal_list) or "".join(line_list) in "".join(diagonal_list)) == False:
-                line_set = 0
+                diagonal_set = 0
             else:
                 print(f" {player} WINS1")
             if columns == diagonal_logic+span and board[rows][columns] == player:
