10f59ebbf4c27eb44a8615dcbdb366eb5af83a12
jason klutse <jklutse@uoguelph.ca>
2026-09-15 22:54:42 -0400

    cleaned function line check

--- game_logic.py
+++ game_logic.py
@@ -11,19 +11,23 @@ def check_win(board, player, x, y):
             line_check(types, player, board, x, y, span)
 
 def line_check(type, player, board, x, y, span):
-
     sub_board = [[row[i] for row in board] for i in range(len(board[0]))]
     line_list = [player]*game1.win_amount
     anti_diagonal_list = diagonal_list = []
 
     for set_1 in range(0, y if type else x):
         for set_2 in range(0, x if type else y):
+
             column = set_1 if type else set_2
+
             if column == set_1 and type == 0:
                 diagonal_list.append(board[set_1+span][column])
+                
             if column == set_2 and type == 1:
                 anti_diagonal_list.append(board[abs(x-(set_2+1))+span][column])
+
             board_list = ["".join(board[column]), "".join(sub_board[column]),"".join(diagonal_list),"".join(anti_diagonal_list)]
+
             for i in range(4):
                 if ("".join(line_list) in "".join(board_list[i])) == True:
                     win_game(player)
