0a2f3b8040d1978a64de6e6bcc7e48634ac29e89
jason klutse <jklutse@uoguelph.ca>
2026-09-15 22:51:41 -0400

    added list to with loop to shorten if statement

--- game_logic.py
+++ game_logic.py
@@ -14,9 +14,8 @@ def line_check(type, player, board, x, y, span):
 
     sub_board = [[row[i] for row in board] for i in range(len(board[0]))]
     line_list = [player]*game1.win_amount
-    anti_diagonal_list = []    
-    diagonal_list = []
-    
+    anti_diagonal_list = diagonal_list = []
+
     for set_1 in range(0, y if type else x):
         for set_2 in range(0, x if type else y):
             column = set_1 if type else set_2
@@ -24,13 +23,11 @@ def line_check(type, player, board, x, y, span):
                 diagonal_list.append(board[set_1+span][column])
             if column == set_2 and type == 1:
                 anti_diagonal_list.append(board[abs(x-(set_2+1))+span][column])
-
-            if ("".join(line_list) in "".join(anti_diagonal_list) or "".join(line_list) in "".join(diagonal_list) or "".join(line_list) in "".join(board[set_2 if type else set_1]) or "".join(line_list) in "".join(sub_board[column])) == True:
-                win_game(player)
+            board_list = ["".join(board[column]), "".join(sub_board[column]),"".join(diagonal_list),"".join(anti_diagonal_list)]
+            for i in range(4):
+                if ("".join(line_list) in "".join(board_list[i])) == True:
+                    win_game(player)
             
-                
-
-
 def win_game(player):
     print(f" \x1b[{5+game1.y};{3+int(game1.x/2)}H{player} WINS")
 
